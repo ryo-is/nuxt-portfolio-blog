8524869f8f3e4d78a155aa14fcfc484e04dcc905
is_ryo <izumiryousuke@izumiryousukenoMacBook-Pro.local>
2020-05-06 00:53:29 +0900

    add:add svg icons

--- pages/info.vue
+++ pages/info.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
-  .info-page
-    img.photo.mt-12(src="../assets/my_photo.png")
+  .info-page.d-flex.flex-column.justify-center.align-center(style="height: calc(100vh - 54px);")
+    img.photo(src="../assets/my_photo.png")
     .name.display-2.text-center.font-weight-bold.mt-10 Ryosuke Izumi
     .job.display-1.text-center.mt-6 WebApplication / IoT
     .likes.display-1.text-center.mt-6 AWS / Vue / TypeScript / Serverless
     .community.display-1.text-center.mt-6 v-kansai / kansai.ts
+
 </template>
 
 <script lang="ts">
--- pages/skill.vue
+++ pages/skill.vue
@@ -1,20 +1,41 @@
 <template lang="pug">
-  .skill.pb-12
-    v-row(justify="center")
-      v-col(cols=12 md=10 lg=8)
-        .skill-set.display-2.font-weight-bold.my-12 SkillSet
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 AWS
-        .skill-set-article.title 使い始めて4年程度。好きなサービスは Lambda / DynamoDB / AppSync あたり。
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 HTML / CSS / JavaScript
-        .skill-set-article.title 使い始めて5年程度。CSSアニメーションとかが好き。
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Pug / Sass(Scss) / TypeScript
-        .skill-set-article.title 使い始めて3年程度。もう型のない世界には戻れない。
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Node.js
-        .skill-set-article.title 使い始めて3年程度。サーバサイドの言語として使っています。<br>また Lambda のランタイム言語としてもよく使っています。JS のまま書いていたり TS で書いたりしています。
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Vue.js / Nuxt.js
-        .skill-set-article.title 使い始めて2年程度。Webアプリケーションのフレームワークとして採用しています。<br>Vue を書くときは Pug / Scss / TypeScript で書いています。業務でも趣味でも使っています。
-        .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Python
-        .skill-set-article.title Lambdaで使ったり、IoTシステムで使うGWの中で使ったりしています。ただほんとにチョットカケルってレベル。
+  .skill.d-flex.justify-center.align-center(style="height: calc(100vh - 54px);")
+    v-col(cols=12 md=10 lg=8)
+      .skill-set.display-2.font-weight-bold.mb-12 My SkillSet
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 AWS
+      //- .skill-set-article.title 使い始めて4年程度。好きなサービスは Lambda / DynamoDB / AppSync あたり。
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 HTML / CSS / JavaScript
+      //- .skill-set-article.title 使い始めて5年程度。CSSアニメーションとかが好き。
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Pug / Sass(Scss) / TypeScript
+      //- .skill-set-article.title 使い始めて3年程度。もう型のない世界には戻れない。
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Node.js
+      //- .skill-set-article.title 使い始めて3年程度。サーバサイドの言語として使っています。<br>また Lambda のランタイム言語としてもよく使っています。JS のまま書いていたり TS で書いたりしています。
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Vue.js / Nuxt.js
+      //- .skill-set-article.title 使い始めて2年程度。Webアプリケーションのフレームワークとして採用しています。<br>Vue を書くときは Pug / Scss / TypeScript で書いています。業務でも趣味でも使っています。
+      //- .skill-set-title.display-1.font-weight-bold.mt-6.mb-4 Python
+      //- .skill-set-article.title Lambdaで使ったり、IoTシステムで使うGWの中で使ったりしています。ただほんとにチョットカケルってレベル。
+      v-row.justify-center.align-center
+        v-col(cols=1)
+          img.skill-icon(src="../assets/html5-original.svg", alt="html5")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/css3-original.svg", alt="css3")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/sass-original.svg", alt="sass")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/javascript-original.svg", alt="javascript")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/typescript-original.svg", alt="typescript")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/vuejs-original.svg", alt="vuejs")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/nodejs-original-wordmark.svg", alt="nodejs")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/python-original.svg", alt="python")
+      v-row.justify-center.align-center
+        v-col(cols=1)
+          img.skill-icon(src="../assets/aws-2.svg", alt="aws")
+        v-col(cols=1)
+          img.skill-icon(src="../assets/firebase-1.svg", alt="firebase")
 </template>
 
 <script lang="ts">
@@ -27,6 +48,9 @@ export default Vue.extend({})
 .skill {
   .title-text {
     font-size: 2.5rem;
+  }
+  .skill-icon {
+    width: 100%;
   }
 }
 </style>
