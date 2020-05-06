1ef7e7871d1419fa12a31d0a303c0673f9e074e6
is_ryo <izumiryousuke@izumiryousukenoMacBook-Pro.local>
2020-05-06 22:37:37 +0900

    mod:change about text

--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,8 @@
   .about-page.d-flex.flex-column.justify-center.align-center(style="height: calc(100vh - 54px);")
     img.photo(src="../assets/my_photo.png")
     .name.display-2.text-center.font-weight-bold.mt-10 Ryosuke Izumi
-    .job.display-1.text-center.mt-6 WebApplication / IoT
-    .likes.display-1.text-center.mt-6 AWS / Vue / TypeScript / Serverless
+    .job.display-1.text-center.mt-6 Frontend / WebApplication
+    .likes.display-1.text-center.mt-6 Vue (Nuxt) / TypeScript / AWS / Serverless
     .community.display-1.text-center.mt-6 v-kansai / kansai.ts
 
 </template>
